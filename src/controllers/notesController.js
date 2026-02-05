2475e719e00be2ad390d8b795e4b06503bf32d11
GremlenA <pedenkoartem5@gmail.com>
2026-02-05 17:48:25 +0200

    8

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -3,24 +3,23 @@ import createHttpError from 'http-errors';
 
 export const getAllNotes = async (req, res, next) => {
   try {
-    const { _id: userId } = req.user;
+    const { _id: userId } = req.user; // Отримуємо ID юзера
     const { page = 1, perPage = 10, tag, search } = req.query;
 
-
+    // 1. Створюємо запит, одразу фільтруючи по власнику (userId)
     const notesQuery = Note.find({ userId });
 
-
+    // 2. Додаємо фільтр по тегу (ланцюжком)
     if (tag) {
       notesQuery.where('tag').equals(tag);
     }
 
-
+    // 3. Додаємо пошук по тексту (ланцюжком, не переоголошуючи змінну)
     if (search) {
-
       notesQuery.where({ $text: { $search: search } });
     }
 
-
+    // Пагінація
     const limit = Number(perPage);
     const pageNumber = Number(page);
     const skip = (pageNumber - 1) * limit;
@@ -53,7 +52,7 @@ export const getNoteById = async (req, res, next) => {
     const { noteId } = req.params;
     const { _id: userId } = req.user;
 
-
+    // 4. Виправлено: findById -> findOne (+ userId)
     const note = await Note.findOne({ _id: noteId, userId });
 
     if (!note) {
@@ -74,7 +73,7 @@ export const createNote = async (req, res, next) => {
   try {
     const { _id: userId } = req.user;
 
-
+    // 5. Виправлено: додаємо userId до тіла нотатки
     const note = await Note.create({
       ...req.body,
       userId,
@@ -96,7 +95,7 @@ export const deleteNote = async (req, res, next) => {
     const { noteId } = req.params;
     const { _id: userId } = req.user;
 
-
+    // 6. Виправлено: findByIdAndDelete -> findOneAndDelete (+ userId)
     const note = await Note.findOneAndDelete({ _id: noteId, userId });
 
     if (!note) {
@@ -114,6 +113,7 @@ export const updateNote = async (req, res, next) => {
     const { noteId } = req.params;
     const { _id: userId } = req.user;
 
+    // 7. Виправлено: findByIdAndUpdate -> findOneAndUpdate (+ userId)
     const note = await Note.findOneAndUpdate(
       { _id: noteId, userId },
       req.body,
