45e99245c8f558624882e31fda7d9b37967b5f5b
GremlenA <pedenkoartem5@gmail.com>
2026-02-05 17:42:44 +0200

    6

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,32 +1,27 @@
 import { Router } from "express";
 import { celebrate } from "celebrate";
 import { authenticate } from "../middleware/authenticate.js";
-
-
 import {
   createNote,
   deleteNote,
   getAllNotes,
   getNoteById,
-  updateNote
+  updateNote,
 } from "../controllers/notesController.js";
-
-
 import {
   createNoteSchema,
   updateNoteSchema,
   getAllNotesSchema,
-  noteIdSchema
+  noteIdSchema,
 } from "../validations/notesValidation.js";
 
 const router = Router();
 
 router.use(authenticate);
 
+
 router.post("/", celebrate(createNoteSchema), createNote);
 router.get("/", celebrate(getAllNotesSchema), getAllNotes);
-
-
 router.get("/:noteId", celebrate(noteIdSchema), getNoteById);
 router.patch("/:noteId", celebrate(updateNoteSchema), updateNote);
 router.delete("/:noteId", celebrate(noteIdSchema), deleteNote);
