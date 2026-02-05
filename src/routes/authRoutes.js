05564a8ea02907995a5d1c50e4ae5e35a35c3f5b
GremlenA <pedenkoartem5@gmail.com>
2026-02-05 17:07:34 +0200

    2

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { celebrate } from "celebrate";
-import { loginUserSchema, registerUserShcema } from "../validations/authValidation.js";
+import { loginUserSchema, registerUserSchema } from "../validations/authValidation.js";
 import { loginUser, registerUser,logoutUser,refreshUserSession} from "../controllers/authController.js";
 
 
 const router = Router();
 
-router.post("/auth/register",celebrate(registerUserShcema),registerUser);
+router.post("/auth/register",celebrate(registerUserSchema),registerUser);
 router.post("/auth/login",celebrate(loginUserSchema),loginUser);
 router.post('/auth/logout', logoutUser);
 router.post('/auth/refresh', refreshUserSession);
